439019df5b2b4715ccf6750b51c27be77b3753be
ShowGa <peter1997217123@gmail.com>
2025-02-26 23:11:44 +0800

    <Initialize> 1. Initialize the project

---
+++ client/src/goObject/Board.ts
@@ -0,0 +1,27 @@
+export type Stone = "b" | "w";
+
+export class Board {
+  private size: number;
+  private state: (Stone | null)[][];
+
+  constructor(size: number) {
+    this.size = size;
+    this.state = Array.from({ length: size }, () =>
+      Array.from({ length: size }, () => null)
+    );
+  }
+
+  // check if the stone was able to be placed based on position occupied or not
+  placeStone(row: number, col: number, color: Stone): boolean {
+    // check if placement exceed the board margin
+    if (row < 0 || row >= this.size || col < 0 || row >= this.size) {
+      return false;
+    }
+
+    // check if the target position been occupied
+    if (this.state[row][col] !== null) {
+    }
+
+    return true;
+  }
+}
