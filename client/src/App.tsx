6c5f4e168356388ced04104bbe3d6e971d94029b
ShowGa <peter1997217123@gmail.com>
2025-03-02 00:19:33 +0800

    <Progress> 1. Create Login, SignUp page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout1 from "./layouts/Layout1";
 import Battle from "./pages/Battle";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Without Layout */}
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        {/* With layout */}
         <Route element={<Layout1 />}>
           <Route path="/play/:id" element={<Battle />} />
         </Route>
