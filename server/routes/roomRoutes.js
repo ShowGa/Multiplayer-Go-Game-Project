d2b07e3cdc250698957dc127c0e4802c4bf4557f
ShowGa <peter1997217123@gmail.com>
2025-03-03 18:31:37 +0800

    <Progress> 1. Setup redis and basic room create route structure

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,10 +1,39 @@
 import { Router } from "express";
 import { v4 as uuidv4 } from "uuid";
+import redisClient from "../redis/redisClient.js";
 
 const router = Router();
 
-router.post("/create-room", (req, res) => {
-  const roomId = uuidv4();
+router.post("/create-room", async (req, res) => {
+  try {
+    const { boardState } = req.body;
+
+    const roomId = uuidv4();
+
+    const data = {
+      players: {
+        // === Modify Later : When login functionality done === //
+        black: { userId: "haha" },
+        white: { userId: "hehe" },
+      },
+      boardState: boardState,
+      currentTurn: "black",
+      status: "matching",
+    };
+
+    // store into cache
+    await redisClient.hSet(`Room:${roomId}`, {
+      players: JSON.stringify(data.players),
+      boardState: JSON.stringify(data.boardState),
+      currentTurn: data.currentTurn,
+      status: data.status,
+    });
+
+    return res.json({ roomId, ...data });
+  } catch (e) {
+    console.error("Error creating room:", e.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export default router;
